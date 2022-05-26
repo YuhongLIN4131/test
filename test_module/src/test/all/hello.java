db20df3bee78342b3cbb4ec999943a92e7239c46
YuhongLIN4131 <1450422779@qq.com>
2022-05-26 15:34:05 +0800

    第一次吃

---
+++ test_module/src/test/all/hello.java
@@ -0,0 +1,8 @@
+package test.all;//表明包的归属
+import java.util.*;
+public class hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("第二个测试");
+    }
+}
